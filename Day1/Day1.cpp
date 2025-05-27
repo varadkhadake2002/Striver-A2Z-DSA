025f088065c5cc78189955cbb9f911f28a8bde4e
Varad <dev.varadk@gmail.com>
2025-05-27 09:44:11 +0000

    Completed basic data types practic on Day1

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 int main(){
-    int x;
-    cout << "Enter a number: "<< endl;
-    cin>>x;
-    cout << "You entered: " << x << endl;
+    
+    int x, y;
+    cout << "Enter Values for X and y: ";
+    cin >> x >> y;
+    cout << "X value is " << x << "\n" << "Y value is " << y << endl;
+
+
     return 0;
 }
