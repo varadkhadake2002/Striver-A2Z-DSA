4d16b872906b0d68e74113d1e415e2ddde92b385
Varad <dev.varadk@gmail.com>
2025-06-03 20:04:58 +0000

    Added pattern 10: Right-Angled Triangle with Alphabets

---
+++ Day2/Pattern10.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    //=========== Right-Angled Triangle with Alphabets =========
+
+    int rows;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i=1; i<=rows; i++){
+        char ch = 'A';
+
+        for(int j=1; j<=i;j++){
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
+
+
