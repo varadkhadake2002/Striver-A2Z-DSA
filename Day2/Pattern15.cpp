bb67fd0b6ee5e48455426806f07fef18823c474e
Varad <dev.varadk@gmail.com>
2025-06-04 12:17:14 +0000

    Added Pattern15: Right-Angled Triangle with Continuous Alphabets (A B C D...)

---
+++ Day2/Pattern15.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    //=========== Right-Angled Triangle with Continuous Alphabets (A B C D...) =========
+
+    int rows;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    char ch = 'A';
+
+    for(int i = 1; i<=rows; i++){
+        
+        for(int j = 1; j<=i; j++){
+            cout << ch << " ";
+            ch++;
+        }
+        
+        cout << endl;
+    }
+
+    return 0;
+}
