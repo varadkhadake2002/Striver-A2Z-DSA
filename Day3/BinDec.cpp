272bcfe3f7fbbd38e1a91a9027df5b56ac428ecb
Varad <dev.varadk@gmail.com>
2025-06-11 19:46:36 +0000

    Auto-commit on 2025-06-11 19:46:36

--- Day3/BinDec.cpp
+++ Day3/BinDec.cpp
@@ -8,6 +8,8 @@ int DecToBin(int decNum){
     while(decNum > 0){
         int rem = decNum % 2;
         decNum /= 2;
+
+        ans += (rem*power);
         power *= 10;
     }
 
@@ -19,6 +21,12 @@ int main(){
     int decNum = 50;
 
     cout << "The decimal form of 50 is: " << DecToBin(decNum) << endl;
+
+    cout << "The decimal form of 1-10 is: " << "\n";
+
+    for(int i = 1; i<=10; i++){
+        cout << i << " : "<< DecToBin(i) << endl;
+    }
 
     return 0;
 
