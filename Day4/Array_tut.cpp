f7348cc8d309481409d4fb075b6b22d232486bd1
Varad <dev.varadk@gmail.com>
2025-06-13 20:38:28 +0000

    Auto-commit on 2025-06-13 20:38:28

---
+++ Day4/Array_tut.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int marks [5] = {99,100,54,36,88}; // We can assign value too for each of the block.
+
+    double price[] = {98.99,105.67,30.00}; // 3, It automatically detects / counts the assigned values and set the size.
+
+
+    cout << marks[1] << "\n";
+    cout << marks[2] << "\n";
+    cout << marks[3] << "\n";
+    cout << marks[4] << "\n";
+    cout << marks[0] << "\n";
+    cout << marks[5] << "\n"; // HERE IT WILL PRINT A GARBAGE VALUE CAUSE IT'S EXCEEDING THE INDEXING FOR THE GIVEN ARRAY.
+
+    return 0;
+
+}
