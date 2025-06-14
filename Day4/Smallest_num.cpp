e6b0b2853357da600d066d734dd1a654bd9ac9ac
Varad <dev.varadk@gmail.com>
2025-06-14 10:04:00 +0000

    Auto-commit on 2025-06-14 10:04:00

--- Day4/Smallest_num.cpp
+++ Day4/Smallest_num.cpp
@@ -5,7 +5,7 @@ int main(){
 
     //------------------------------------------ TAKING INPUT FROM USER ----------------------------------------------------------//
 
-    int sze;
+    int sze, inx;
     int smz = INT_MAX;
     cout << "Enter the size of array you want to print: ";
     cin >> sze;
@@ -21,12 +21,13 @@ int main(){
 
         if(arr[i]<smz){
             smz = arr[i];
+            inx = i;
         }
 
     }
 
     cout << "The smallest number is" << smz << "\n";
-
+    cout << "The smallest number index is " << inx << "\n";
 
 
 
@@ -36,18 +37,20 @@ int main(){
     int marks [5] = {72, 11 , -2, 20, -12};
 
     int sz = sizeof(marks)/sizeof(int);
-    int sm = INT_MAX;
+    int sm = INT_MAX, index;
 
     for(int i = 0; i<sz; i++){
 
         if(marks[i]<sm){
             sm = marks[i];
+            index = i;
         }
 
     }
     
 
     cout << "The smallest number is" << sm << "\n";
+    cout << "The smallest number index is " << index << "\n";
 
     return 0;
 }
